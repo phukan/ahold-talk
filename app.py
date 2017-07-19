33fcdbf78dd6fd211df066fbc2b02517bf795aa0
phukan <ujjalphukan@gmail.com>
2017-07-19 11:04:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,25 +29,6 @@ def webhook():
         return 'foo'
 
 
-
-def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
-    speech = "The cost of shipping to " + str(zone) + " is " + str(cost[zone]) + " euros."
-    print("Response:")
-    print(speech)
-    return {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        # "contextOut": [],
-        "source": "apiai-onlinestore-shipping"
-    }
-
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
